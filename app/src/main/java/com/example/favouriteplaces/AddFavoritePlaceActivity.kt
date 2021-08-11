fabde668602056d78e1bc06278e4eb1638d9beb3
Abhas Kumar <abhas1102@gmail.com>
2021-08-12 03:01:14 +0530

    Setup the app theme and fab button by importing Material Design

---
+++ app/src/main/java/com/example/favouriteplaces/AddFavoritePlaceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.favouriteplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddFavoritePlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_favorite_place)
+    }
+}
