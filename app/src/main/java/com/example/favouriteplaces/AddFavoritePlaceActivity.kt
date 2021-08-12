3485335fce8e0ae2c6978fb2fe4742339f581a37
Abhas Kumar <abhas1102@gmail.com>
2021-08-12 19:36:53 +0530

    Added custom toolbar to AddPlace screen

--- app/src/main/java/com/example/favouriteplaces/AddFavoritePlaceActivity.kt
+++ app/src/main/java/com/example/favouriteplaces/AddFavoritePlaceActivity.kt
@@ -2,10 +2,17 @@ package com.example.favouriteplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_favorite_place.*
 
 class AddFavoritePlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_favorite_place)
+
+        setSupportActionBar(toolbar_add_place)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar_add_place.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
