5c8b459b12db0d9fa84d42f863d669644243fbfd
Abhas Kumar <abhas1102@gmail.com>
2021-08-16 03:36:01 +0530

    Used third party library Dexter for Multiple Permissions Listener.

--- app/src/main/java/com/example/favouriteplaces/AddFavoritePlaceActivity.kt
+++ app/src/main/java/com/example/favouriteplaces/AddFavoritePlaceActivity.kt
@@ -2,14 +2,24 @@ package com.example.favouriteplaces
 
 import android.app.AlertDialog
 import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
-import com.google.android.material.datepicker.MaterialDatePicker
+import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.SettingsClickListener
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.activity_add_favorite_place.*
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class AddFavoritePlaceActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -26,9 +36,9 @@ class AddFavoritePlaceActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-            cal.set(Calendar.YEAR,year)
-            cal.set(Calendar.MONTH,month)
-            cal.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
             updateDateInView()
         }
 
@@ -41,19 +51,19 @@ class AddFavoritePlaceActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when(v!!.id){
             R.id.et_date -> {
-                DatePickerDialog(this@AddFavoritePlaceActivity,dateSetListener,cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show()
+                DatePickerDialog(this@AddFavoritePlaceActivity, dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
             }
 
-            R.id.iv_place_image ->{
+            R.id.tv_add_image -> {
                 val picktureDialog = AlertDialog.Builder(this) // Using builder as context this to build Alert Dialog
                 picktureDialog.setTitle("Select Action")
-                val pictureDialogItems = arrayOf("Select Photo from Gallery","Capture photo from Camera")
-                picktureDialog.setItems(pictureDialogItems){ //Using picture dialog to set it's items
+                val pictureDialogItems = arrayOf("Select Photo from Gallery", "Capture photo from Camera")
+                picktureDialog.setItems(pictureDialogItems) { //Using picture dialog to set it's items
                     dialog, which ->    // Defining some action depending on dialog which is selected
-                    when(which){
+                    when (which) {
                         0 -> choosePhotoFromGallery()
-                        1 -> Toast.makeText(this@AddFavoritePlaceActivity,"Camera selction coming soon",
-                    Toast.LENGTH_SHORT).show()
+                        1 -> Toast.makeText(this@AddFavoritePlaceActivity, "Camera selction coming soon",
+                                Toast.LENGTH_SHORT).show()
                     }
                 }
                 picktureDialog.show() //Showing the dialogue
@@ -64,7 +74,42 @@ class AddFavoritePlaceActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun choosePhotoFromGallery(){
+    Dexter.withActivity(this).withPermissions(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 
+    ).withListener(object: MultiplePermissionsListener{
+        override fun onPermissionsChecked(report: MultiplePermissionsReport?){
+            if (report!!.areAllPermissionsGranted()){
+                Toast.makeText(this@AddFavoritePlaceActivity,"Storage Read Write Permission Granted",
+                Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>, token: PermissionToken) { // To show user why it is needed
+            showRationalDialogForPermissions()
+        }
+    }).onSameThread().check()
+    }
+
+    private fun showRationalDialogForPermissions(){
+        AlertDialog.Builder(this).setMessage("It looks like you have " +
+                "turned off permissions").setPositiveButton("Go to Settings")
+        { _,_ ->
+            try {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package",packageName,null) // Sending the user to Settings
+                intent.data = uri //Adding the uri data to intent
+                startActivity(intent)
+
+            }catch (e:ActivityNotFoundException){
+                e.printStackTrace()
+            }
+
+        }.setNegativeButton("cancel"){dialog,which ->
+            dialog.dismiss()
+        }.show()
     }
 
     private fun updateDateInView(){
