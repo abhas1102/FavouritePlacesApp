e6e658abcab6dc42351f747931bf7c61da0b59ed
Abhas Kumar <abhas1102@gmail.com>
2021-08-26 02:45:58 +0530

    getting database data on main activity to show the places list.

--- app/src/main/java/com/example/favouriteplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/favouriteplaces/activities/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.favouriteplaces.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.favouriteplaces.R
+import com.example.favouriteplaces.database.DatabaseHandler
+import com.example.favouriteplaces.models.FavoritePlaceModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,18 @@ class MainActivity : AppCompatActivity() {
         fabAddHappyPlace.setOnClickListener{
             val intent = Intent(this, AddFavoritePlaceActivity::class.java)
             startActivity(intent)
+        }
+        getFavoritePlacesListFromLocalDB()
+    }
+
+    private fun getFavoritePlacesListFromLocalDB(){
+        val dbHandler = DatabaseHandler(this)
+        val getFavoritePlaceList : ArrayList<FavoritePlaceModel> = dbHandler.getFavoritePlacesList()
+
+        if (getFavoritePlaceList.size>0){
+            for(i in getFavoritePlaceList){
+                Log.e("Title", i.title)
+            }
         }
     }
 }
