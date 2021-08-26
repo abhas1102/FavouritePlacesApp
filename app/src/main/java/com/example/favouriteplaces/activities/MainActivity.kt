0d82e408722a7f10c802ec38ac25f5458dc55a55
Abhas Kumar <abhas1102@gmail.com>
2021-08-27 02:08:41 +0530

    Attached recycler view to adapter to display list items.

--- app/src/main/java/com/example/favouriteplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/favouriteplaces/activities/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.favouriteplaces.R
+import com.example.favouriteplaces.adapter.FavoritePlacesAdapter
 import com.example.favouriteplaces.database.DatabaseHandler
 import com.example.favouriteplaces.models.FavoritePlaceModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,14 +24,28 @@ class MainActivity : AppCompatActivity() {
         getFavoritePlacesListFromLocalDB()
     }
 
+    private fun setupFavoritePlacesRecyclerView(favoritePlaceList:ArrayList<FavoritePlaceModel>){
+        rv_favorite_places_list.layoutManager = LinearLayoutManager(this)
+        rv_favorite_places_list.setHasFixedSize(true)
+        val placesAdapter = FavoritePlacesAdapter(this,favoritePlaceList)
+        rv_favorite_places_list.adapter = placesAdapter
+    }
+
     private fun getFavoritePlacesListFromLocalDB(){
         val dbHandler = DatabaseHandler(this)
         val getFavoritePlaceList : ArrayList<FavoritePlaceModel> = dbHandler.getFavoritePlacesList()
 
         if (getFavoritePlaceList.size>0){
-            for(i in getFavoritePlaceList){
-                Log.e("Title", i.title)
-            }
+          //  for(i in getFavoritePlaceList){
+              //  Log.e("Title", i.title)
+           // }
+
+            rv_favorite_places_list.visibility = View.VISIBLE
+            tv_no_records_available.visibility = View.GONE
+            setupFavoritePlacesRecyclerView(getFavoritePlaceList)
+        }else{
+            rv_favorite_places_list.visibility = View.GONE
+            tv_no_records_available.visibility = View.VISIBLE
         }
     }
 }
