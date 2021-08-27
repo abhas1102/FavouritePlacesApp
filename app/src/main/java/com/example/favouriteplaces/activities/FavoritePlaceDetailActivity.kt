b26110f6e8d55f9683bf043730c55d40195d0043
Abhas Kumar <abhas1102@gmail.com>
2021-08-28 02:12:21 +0530

    Created another activity for showing details of recycler view items on clicking. Made changes in manifests

---
+++ app/src/main/java/com/example/favouriteplaces/activities/FavoritePlaceDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.favouriteplaces.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.favouriteplaces.R
+
+class FavoritePlaceDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite_place_detail)
+    }
+}
