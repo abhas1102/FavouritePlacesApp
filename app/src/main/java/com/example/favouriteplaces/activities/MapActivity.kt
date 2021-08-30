ffdac3cec75f273acb51baa0aeb5c793638c4c74
Abhas Kumar <abhas1102@gmail.com>
2021-08-30 20:17:07 +0530

    Setup toolbar on map activity and checked if any information is received from Main Activity

--- app/src/main/java/com/example/favouriteplaces/activities/MapActivity.kt
+++ app/src/main/java/com/example/favouriteplaces/activities/MapActivity.kt
@@ -3,10 +3,40 @@ package com.example.favouriteplaces.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.favouriteplaces.R
+import com.example.favouriteplaces.models.FavoritePlaceModel
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import kotlinx.android.synthetic.main.activity_map.*
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(),OnMapReadyCallback {
+
+    private var mFavoritePlaceDetails:FavoritePlaceModel?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        if (intent.hasExtra(MainActivity.EXTRA_PLACE_DETAILS)){
+
+            mFavoritePlaceDetails = intent.getSerializableExtra(MainActivity.EXTRA_PLACE_DETAILS) as FavoritePlaceModel
+
+
+        }
+
+        if(mFavoritePlaceDetails!=null){
+            setSupportActionBar(toolbar_map)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.title = mFavoritePlaceDetails!!.title
+            toolbar_map.setNavigationOnClickListener {
+                onBackPressed()
+            }
+
+            val supportMapFragment:SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+            supportMapFragment.getMapAsync(this)
+        }
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        TODO("Not yet implemented")
     }
 }
